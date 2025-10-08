fca5105766e39ff7ce28499963675bf1641a5d88
Neila Miranda <161175405+BellaTheAlien@users.noreply.github.com>
2025-10-08 20:56:05 +0000

    replaced the setInterval for a rquestAnimation; created a function called animate (Step 4 complete)

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ document.body.innerHTML = `
     <p>Counter: <span id="counter">0</span> pumpkins</p>
   </div>
 `;
+//section project - interactivity
 console.log("herro");
 console.log(" 'ello ");
+
 const button = document.createElement("button");
 let counter = 0;
-setInterval(incrementClick, 1000);
+//setInterval(incrementClick, 1000);
 button.textContent = "🎃";
 button.id = "clicker";
 document.body.append(button);
@@ -25,3 +27,28 @@ function incrementClick() {
   document.getElementById("counter")!.textContent = counter.toString();
   console.log("Button clicked", counter);
 }
+
+//step 4 - animation loop
+let lastTime = 0;
+const incremtPerSecond = 1;
+
+function animate(timeStamp: number) {
+
+  if (!lastTime) {
+    lastTime = timeStamp;
+    requestAnimationFrame(animate);
+    return;
+  }
+
+  const delta = timeStamp - lastTime;
+  const increment = (delta / 1000) * incremtPerSecond;
+  
+  counter += increment;
+  document.getElementById("counter")!.textContent = Math.floor(counter).toString();
+  lastTime = timeStamp;
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
+
+
