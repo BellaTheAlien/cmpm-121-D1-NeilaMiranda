3d339a807ef5bd6ad9d12d1806fc219a90b91dfa
Neila Miranda <161175405+BellaTheAlien@users.noreply.github.com>
2025-10-08 02:35:38 +0000

    added a pumpkin emoji button checking the commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const button = document.createElement("button");
+button.textContent = "🎃";
+button.id = "clicker";
+document.body.appendChild(button);
