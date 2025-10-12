7b7e4f7b0a60ed2314d8602207e69ea0b8bce8f6
Neila Miranda <161175405+BellaTheAlien@users.noreply.github.com>
2025-10-12 02:51:36 +0000

    renamed the clicker variable into pumpkin seeds, the theme of the clicker has always been pumpkins, added some comments and labels (Step 8 complete)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@
 import "./style.css";
 
 //had some help from Gemini to clean up/understand the HYML style section
+//created a counter area, then an instructions area
+//then the buttons will be added via type script
 document.body.innerHTML = `
   <div class = "game-container">
     <div class="counter-area">
@@ -17,26 +19,28 @@ document.body.innerHTML = `
     
   </div>
 `;
+
 //section project - interactivity
 console.log("herro");
 console.log(" 'ello ");
 
-//the buttons and displays
+//the pumpkin button and display for counter and rate of pumpkins
 const pumpkin_button = document.createElement("button");
 const rate_of_pumpkins = document.getElementById("rate-of-pumpkins")!;
 const counter_display = document.getElementById("counter")!;
 
 //the update/buy buttons
-const clicker_buyer = document.createElement("button");
+const pumpkin_seed_buyer = document.createElement("button");
 const psl_buyer = document.createElement("button");
 const pumpikn_pach = document.createElement("button");
 
+//the counter and rate of pumpkins variables
 let counter = 0;
 let last_time_stamp = 0;
 let incremt_per_second = 0;
 
-//starting upgrade price
-let clicker_price = 10;
+//starting upgrade price variables
+let seed_price = 10;
 let psl_price = 100;
 let pumpkin_patch_price = 1000;
 
@@ -45,44 +49,50 @@ pumpkin_button.textContent = "🎃";
 pumpkin_button.id = "clicker";
 document.body.append(pumpkin_button);
 
-//buy clicker button
-clicker_buyer.textContent = "Buy Pumpkin Seeds (" + clicker_price +
+//buy clicker button - auto clicker
+pumpkin_seed_buyer.textContent = "Buy Pumpkin Seeds (" + seed_price +
   " pumpkins)";
-clicker_buyer.id = "clicker_buyer";
-clicker_buyer.disabled = true;
-document.body.append(clicker_buyer);
+pumpkin_seed_buyer.id = "clicker_buyer";
+pumpkin_seed_buyer.disabled = true;
+document.body.append(pumpkin_seed_buyer);
 
-//buy pls button
+//buy pls button - pumpkin spice latte auto clicker
 psl_buyer.textContent = "Buy a Pumpkin Spice Latte (" + psl_price +
   " pumpkins)";
 psl_buyer.id = "pls_buyer";
 psl_buyer.disabled = true;
 document.body.append(psl_buyer);
 
-//but a pumpkin patch button
+//buy a pumpkin patch button
 pumpikn_pach.textContent = "Buy a Pumpkin Patch (" + pumpkin_patch_price +
   " pumpkins)";
 pumpikn_pach.id = "pumpkin_patch_buyer";
 pumpikn_pach.disabled = true;
 document.body.append(pumpikn_pach);
 
+//event listeners for the pumpkin button and the upgrade buttons
 pumpkin_button.addEventListener("click", () => {
   incrementClick();
 });
 
-clicker_buyer.addEventListener("click", () => {
-  if (counter >= clicker_price) {
-    counter -= clicker_price;
+//buy clicker button event listener
+
+//auto clicker adds 0.1 pumpkins per second
+pumpkin_seed_buyer.addEventListener("click", () => {
+  if (counter >= seed_price) {
+    counter -= seed_price;
     incremt_per_second += 0.1;
-    clicker_price = Math.round((clicker_price * 1.15) * 100) / 100;
-    clicker_buyer.textContent = "Buy Pumpkin Seeds (" +
-      clicker_price.toFixed(2) +
+    seed_price = Math.round((seed_price * 1.15) * 100) / 100;
+    pumpkin_seed_buyer.textContent = "Buy Pumpkin Seeds (" +
+      seed_price.toFixed(2) +
       " pumpkins)";
     console.log("Bought Clicker");
     counter_display.textContent = Math.floor(counter).toString();
   }
 });
 
+//buy psl button event listener
+//psl auto clicker adds 2 pumpkins per second
 psl_buyer.addEventListener("click", () => {
   if (counter >= psl_price) {
     counter -= psl_price;
@@ -96,6 +106,8 @@ psl_buyer.addEventListener("click", () => {
   }
 });
 
+//buy pumpkin patch button event listener
+//pumpkin patch auto clicker adds 50 pumpkins per second
 pumpikn_pach.addEventListener("click", () => {
   if (counter >= pumpkin_patch_price) {
     counter -= pumpkin_patch_price;
@@ -109,13 +121,16 @@ pumpikn_pach.addEventListener("click", () => {
   }
 });
 
+// the function that increments the counter when the pumpkin button is clicked
 function incrementClick() {
   counter++;
   document.getElementById("counter")!.textContent = counter.toString();
   console.log("Button clicked", counter);
 
-  if (counter >= clicker_price) {
-    clicker_buyer.disabled = false;
+  //check if the counter is enough to buy upgrades
+  //enable buttons if enough counter
+  if (counter >= seed_price) {
+    pumpkin_seed_buyer.disabled = false;
   }
   if (counter >= psl_price) {
     psl_buyer.disabled = false;
@@ -142,10 +157,12 @@ function animattion_loop(timeStamp: number) {
   rate_of_pumpkins.textContent = incremt_per_second.toFixed(1) +
     " pumpkins per second";
 
-  if (counter < clicker_price) {
-    clicker_buyer.disabled = true;
+  //check if the counter is enough to buy upgrades
+  //disable buttons if not enough counter, enable if enough counter
+  if (counter < seed_price) {
+    pumpkin_seed_buyer.disabled = true;
   } else {
-    clicker_buyer.disabled = false;
+    pumpkin_seed_buyer.disabled = false;
   }
   if (counter < psl_price) {
     psl_buyer.disabled = true;
@@ -157,8 +174,6 @@ function animattion_loop(timeStamp: number) {
   } else {
     pumpikn_pach.disabled = false;
   }
-
-  //document.getElementById("counter")!.textContent = Math.floor(counter).toString();
 
   last_time_stamp = timeStamp;
   requestAnimationFrame(animattion_loop);
