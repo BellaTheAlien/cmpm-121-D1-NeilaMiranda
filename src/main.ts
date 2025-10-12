94586f1b94c081c2245f0dc50fb1bc4e01d9b4e3
Neila Miranda <161175405+BellaTheAlien@users.noreply.github.com>
2025-10-12 01:09:57 +0000

    added 3 buy upgrades, a rate of pumpkins display, and clean up the HTML (Step 6 complete) - changes in the HTML where to make sepreate classes for the game instrunctions and game area

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,35 @@
 //<p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 import "./style.css";
 
+//had some help from Gemini to clean up/understand the HYML style section
 document.body.innerHTML = `
-  
-  <div class = "myCounter">
-    <p>Counter: <span id="counter">0</span> pumpkins</p>
+  <div class = "game-container">
+    <div class="counter-area">
+      <p><span id="counter">0</span> pumpkins</p>
+      <p id="rate-of-pumpkins">0.0 pumpkins per second</p>
+      </div>
+
+    <div class="instructions">
+      <p>Click the pumpkin to earn pumpkins!</p>
+      <p>Use pumpkins to buy upgrades below.</p>
+    </div>
+    
   </div>
 `;
 //section project - interactivity
 console.log("herro");
 console.log(" 'ello ");
 
+//the buttons and displays
 const pumpkin_button = document.createElement("button");
+const rate_of_pumpkins = document.getElementById("rate-of-pumpkins")!;
+const counter_display = document.getElementById("counter")!;
+
+//the update/buy buttons
 const clicker_buyer = document.createElement("button");
 const psl_buyer = document.createElement("button");
-const counter_display = document.getElementById("counter")!;
+const pumpikn_pach = document.createElement("button");
+
 
 let counter = 0;
 let last_time_stamp = 0;
@@ -27,7 +42,7 @@ pumpkin_button.id = "clicker";
 document.body.append(pumpkin_button);
 
 //buy clicker button
-clicker_buyer.textContent = "Buy Clicker (10 pumpkins)";
+clicker_buyer.textContent = "Buy Pumpkin Seeds (10 pumpkins)";
 clicker_buyer.id = "clicker_buyer";
 clicker_buyer.disabled = true;
 document.body.append(clicker_buyer);
@@ -37,6 +52,12 @@ psl_buyer.textContent = "Buy a Pumpkin Spice Latte (100 pumpkins)";
 psl_buyer.id = "pls_buyer";
 psl_buyer.disabled = true;
 document.body.append(psl_buyer);
+
+//but a pumpkin patch button
+pumpikn_pach.textContent = "Buy a Pumpkin Patch (1000 pumpkins)";
+pumpikn_pach.id = "pumpkin_patch_buyer";
+pumpikn_pach.disabled = true;
+document.body.append(pumpikn_pach);
 
 pumpkin_button.addEventListener("click", () => {
   incrementClick();
@@ -58,6 +79,15 @@ psl_buyer.addEventListener("click", () => {
   }
 });
 
+pumpikn_pach.addEventListener("click", () => {
+  if (counter >= 1000) {
+    counter -= 1000;
+    incremt_per_second += 50;
+    console.log("Bought Pumpkin Patch");
+    counter_display.textContent = Math.floor(counter).toString();
+  }
+});
+
 function incrementClick() {
   counter++;
   document.getElementById("counter")!.textContent = counter.toString();
@@ -68,6 +98,9 @@ function incrementClick() {
   }
   if (counter >= 100) {
     psl_buyer.disabled = false;
+  }
+  if (counter >= 1000) {
+    pumpikn_pach.disabled = false;
   }
 }
 
@@ -85,6 +118,7 @@ function animattion_loop(timeStamp: number) {
 
   counter += increment;
   counter_display.textContent = Math.floor(counter).toString();
+  rate_of_pumpkins.textContent = incremt_per_second.toFixed(1) + " pumpkins per second";
 
   if (counter < 10) {
     clicker_buyer.disabled = true;
@@ -95,6 +129,11 @@ function animattion_loop(timeStamp: number) {
     psl_buyer.disabled = true;
   } else {
     psl_buyer.disabled = false;
+  }
+  if (counter < 1000) {
+    pumpikn_pach.disabled = true;
+  } else {
+    pumpikn_pach.disabled = false;
   }
 
   //document.getElementById("counter")!.textContent = Math.floor(counter).toString();
